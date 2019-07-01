8279d1506dfe32e6bd592b97b5adbdba854b5ecb
Harshini Sri <ssharshu2@gmail.com>
2019-07-01 14:56:24 +0000

    Added new changes for feature-1 branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,20 @@
 public class HelloWorld 
 { 
-    // Your program begins with a call to main(). 
-//main function
-    // Prints "Hello, World" to the terminal window. 
+    	// Your program begins with a call to main(). 
+	//main function
+    	// Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
+	//Prints Hello world
         System.out.println("Hello, World"); 
-    } 
+	
+	//Prints Welcome to Git in the terminal
+        System.out.println("Welcome to GIT ");
+
+	//Prints Welcome to feature branching workflow in the terminal
+        System.out.println("Welcome to Workflows");
+
+
+    
+} 
 } 
